abf3612c7cef52b8a62cd98c45896325ec61eb94
Jack Baron <yodude107@gmail.com>
2017-08-28 16:34:29 +0100

    Write Main

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 #!/usr/bin/env node
+const path = require('path')
 const program = require('commander')
 const { downloadArray } = require('./download')
 
 program
   .version(require('../package.json').version)
+  .option('-D, --directory <dir>', 'Download Directory')
+  .option('-A --auto-number', 'Number the downloads sequentially')
   .parse(process.argv)
+
+/**
+ * 
+ * @param {program} program Commander Program
+ */
+const main = async program => {
+  let directory = program.directory || path.join(__dirname, '..')
+  let args = program.args
+  let keepFilenames = !program.autoNumber
+  downloadArray(args, directory, keepFilenames)
+}
+
+main(program)
